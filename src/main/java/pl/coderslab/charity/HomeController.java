91456bbe49c2e88b310067bec38472de8c8de795
DESKTOP-72BJ4O7\walcu <mrwalczy@gmail.com>
2021-04-03 20:11:11 +0200

    Service fix

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,5 +1,6 @@
 package pl.coderslab.charity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,6 +9,8 @@ import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.entity.Institution;
 import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
+import pl.coderslab.charity.service.DonationService;
+import pl.coderslab.charity.service.InstitutionService;
 
 import java.util.List;
 
@@ -15,30 +18,29 @@ import java.util.List;
 @Controller
 public class HomeController {
 
-    private InstitutionRepository institutionRepository;
-    private DonationRepository donationRepository;
+    @Autowired
+    private InstitutionService institutionService;
+    @Autowired
+    private DonationService donationService;
 
-    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
-        this.institutionRepository = institutionRepository;
-        this.donationRepository = donationRepository;
-    }
+
 
     @RequestMapping("/")
     public String homeAction(Model model) {
-        model.addAttribute("donationsCount",this.donationRepository.count());
-        model.addAttribute("quantityOfBags",this.donationRepository.quantityOfBags());
+        model.addAttribute("donationsCount",this.donationService.donationsCount());
+        model.addAttribute("quantityOfBags",this.donationService.quantityOfBags());
         return "homePage";
     }
 
 
     @ModelAttribute("institution")
     public List<Institution> institutions() {
-        return this.institutionRepository.findAll();
+        return this.institutionService.findAll();
     }
 
     @ModelAttribute("donation")
     public List<Donation> donations() {
-        return this.donationRepository.findAll();
+        return this.donationService.findAll();
     }
 
 }
--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -1,4 +1,28 @@
 package pl.coderslab.charity.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.Institution;
+import pl.coderslab.charity.service.DonationService;
+import pl.coderslab.charity.service.InstitutionService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/donation")
 public class DonationController {
+
+    @Autowired
+    private DonationService donationService;
+    @Autowired
+    private InstitutionService institutionService;
+
+    @ModelAttribute("institution")
+    public List<Institution> institutions() {
+        return this.institutionService.findAll();
+    }
+
 }
